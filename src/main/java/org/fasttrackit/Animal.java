b5b56ec30c4ff454cc2ab535ee1f3e09bdd4788d
Andrei <andreibuzas@gmail.com>
2020-06-04 14:25:50 +0300

    Animal class added

---
+++ src/main/java/org/fasttrackit/Animal.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Animal {
+
+    String name;
+    int age;
+    int healthLevel;
+    int hungerLevel;
+    int spiritLevel;
+    String favoriteFood;
+    String favoriteActivity;
+
+}
